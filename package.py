079b6f443b0a596c912530a6e0b7e77055408583
alee1 <ashley.lee@dreamworks.com>
2023-05-09 11:09:29 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '10.10'
+    _version = '10.11'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -59,9 +59,9 @@ sconsTargets = {
 }
 
 requires = [
-    'moonray-13.10',
+    'moonray-13.11',
     'openimageio-2',
-    'scene_rdl2-11.10',
+    'scene_rdl2-11.11',
 ]
 
 private_build_requires = [
