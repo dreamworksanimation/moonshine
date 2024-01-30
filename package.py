4e742cfe922674006248e0a6f3e48e11ea7e434a
jmahovsky <jeff.mahovsky@dreamworks.com>
2024-01-30 14:00:52 -0800

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '12.11'
+    _version = '12.12'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -70,9 +70,9 @@ sconsTargets = {
 }
 
 requires = [
-    'moonray-15.11',
+    'moonray-15.12',
     'openimageio-2.3.20.0.x',
-    'scene_rdl2-13.6',
+    'scene_rdl2-13.7',
 ]
 
 private_build_requires = [
