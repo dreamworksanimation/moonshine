5a0b9a4569aa97a471311d70a2b00ac3ac143626
jlanz <Jon.Lanz@dreamworks.com>
2024-02-20 16:28:36 -0800

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '12.13'
+    _version = '12.14'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -70,9 +70,9 @@ sconsTargets = {
 }
 
 requires = [
-    'moonray-15.13',
+    'moonray-15.14',
     'openimageio-2.3.20.0.x',
-    'scene_rdl2-13.7',
+    'scene_rdl2-13.8',
 ]
 
 private_build_requires = [
