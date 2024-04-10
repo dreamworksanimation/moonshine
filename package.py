9c0a044c25b4f84943fb0bc1d78ecc5fe7e6583a
rpacker <randy.packer@dreamworks.com>
2024-04-09 21:20:10 -0700

    Bumped major version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '12.20'
+    _version = '13.0'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -46,9 +46,9 @@ conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
-    'moonray-15.20',
+    'moonray-16.0',
     'openimageio-2.3.20.0.x',
-    'scene_rdl2-13.12',
+    'scene_rdl2-14.0',
 ]
 
 private_build_requires = [
