d823d9b16966a9d849b0721eb1f4c29d1f4df99e
Mike Day <mike.day@dreamworks.com>
2024-06-18 21:10:05 -0700

    Comment out clang-17 debug build

--- package.py
+++ package.py
@@ -41,7 +41,7 @@ variants = [
     ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.0', 'gcc-11.x'],
     ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2023.0', 'gcc-11.x'],
     ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.0', 'clang-17.0.6.x'],
-    ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2023.0', 'clang-17.0.6.x'],
+#    ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2023.0', 'clang-17.0.6.x'],
 ]
 
 conf_rats_variants = variants[0:2]
