591521f8e4c40b793eb28ed0bb5760b7d30300f4
alee1 <ashley.lee@dreamworks.com>
2024-08-27 14:25:38 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '13.19'
+    _version = '13.20'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -48,9 +48,9 @@ conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
-    'moonray-16.19',
+    'moonray-16.20',
     'openimageio-2.3.20.0.x',
-    'scene_rdl2-14.12',
+    'scene_rdl2-14.13',
 ]
 
 private_build_requires = [
