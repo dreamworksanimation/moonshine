ec52de6c5f2745928cc02e0649c0514a150027bf
bart <build.and.release.tools@dreamworks.com>
2024-10-09 11:27:08 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '13.26'
+    _version = '13.27'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -48,9 +48,9 @@ conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
-    'moonray-16.26',
+    'moonray-16.27',
     'openimageio-2.3.20.0.x',
-    'scene_rdl2-14.15',
+    'scene_rdl2-14.16',
 ]
 
 private_build_requires = [
