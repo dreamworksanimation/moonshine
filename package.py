a63b19dcea75333aeafa3c0799792369f38d5a62
206539443_nbcuni <Mike.Day@dreamworks.com>
2025-05-07 09:01:42 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.12'
+    _version = '14.13'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -44,7 +44,7 @@ conf_rats_variants = variants[0:2]
 conf_CI_variants = variants
 
 requires = [
-    'moonray-17.11',
+    'moonray-17.12',
     'scene_rdl2-15.6',
 ]
 
