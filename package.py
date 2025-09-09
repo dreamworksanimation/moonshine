dbc182f0e62cf3005bace2e53c38895bf7b2409c
206531218_nbcuni <toshi.kato@dreamworks.com>
2025-09-09 15:13:26 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.25'
+    _version = '14.26'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -44,8 +44,8 @@ conf_rats_variants = variants[0:2]
 conf_CI_variants = variants
 
 requires = [
-    'moonray-17.23',
-    'scene_rdl2-15.11',
+    'moonray-17.24',
+    'scene_rdl2-15.12',
 ]
 
 private_build_requires = [
